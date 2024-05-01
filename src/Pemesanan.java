118c9f784b02b1060200c537342ba7e8bd1abb52
naufalfr27 <naufalfadhilrizqi27@gmail.com>
2024-05-01 18:41:31 +0700

    first commit

--- src/Pemesanan.java
+++ src/Pemesanan.java
@@ -4,15 +4,33 @@ class Pemesanan {
     private String tahap;
     private Tiket tiket;
     private int jumlah;
+    
 
     public Pemesanan(String nama, String tahap, Tiket tiket, int jumlah) {
         this.nama = nama;
         this.tahap = tahap;
         this.tiket = tiket;
-        this.jumlah = jumlah;
+        this.jumlah = jumlah;        
     }
 
     public void cetakNota() {
         // Do your magic here
+        double totalHarga = tiket.getHarga() * jumlah;
+
+        if (tahap == "Presale"){
+            System.out.println("--- Nota Pemesanan ---");
+            System.out.println("Nama Pembeli: " + nama);
+            System.out.println("Tahap Pembelian: " + tahap);
+            System.out.println("Jenis Tiket: " + tiket.getClass().getSimpleName());
+            System.out.println("Jumlah Tiket: " + jumlah);
+            System.out.println("Total Harga: Rp " + totalHarga * 0.8);
+        } else{
+            System.out.println("--- Nota Pemesanan ---");
+            System.out.println("Nama Pembeli: " + nama);
+            System.out.println("Tahap Pembelian: " + tahap);
+            System.out.println("Jenis Tiket: " + tiket.getClass().getSimpleName());
+            System.out.println("Jumlah Tiket: " + jumlah);
+            System.out.println("Total Harga: Rp " + totalHarga);
+        }
     }
 }
--- src/PesanTiketApp.java
+++ src/PesanTiketApp.java
@@ -7,6 +7,77 @@ public class PesanTiketApp {
         System.out.println("Selamat datang di program pemesanan tiket!");
         
         // Do your magic here
-        
+        System.out.print("Masukan nama pembeli: ");
+        String nama = scanner.nextLine();
+
+        System.out.println("\nPilih tahap pembelian: ");
+        System.out.println("1. Presale");
+        System.out.println("2. Reguler");
+        System.out.print("Pilih tahap pembeli (1/2): ");
+        int tahap = scanner.nextInt();
+
+        if(tahap == 1){
+            System.out.println("\nPilih jenis tiket: ");
+            System.out.println("1. VIP");
+            System.out.println("2. VVIP");
+            System.out.print("Pilih jenis tiket (1/2): ");
+            int jenisTiket = scanner.nextInt();
+            scanner.nextLine();
+
+            if(jenisTiket != 1 && jenisTiket != 2){
+                System.out.println("\nInput tidak valid!");                
+            } else{
+                System.out.print("\nMasukan jumlah tiket yang ingin dibeli: ");
+                int jumlahTiket = scanner.nextInt();
+
+                Tiket tiket;
+                if (jenisTiket == 1) {
+                    tiket = new VIP();
+                    Pemesanan pemesanan = new Pemesanan(nama, "Presale", tiket, jumlahTiket);
+                    pemesanan.cetakNota();
+                } else if (jenisTiket == 2) {
+                    tiket = new VVIP();
+                    Pemesanan pemesanan = new Pemesanan(nama, "Presale", tiket, jumlahTiket);
+                    pemesanan.cetakNota();
+                } 
+            }
+
+        } else if(tahap == 2){
+            System.out.println("\nPilih jenis tiket: ");
+            System.out.println("1. Festival");
+            System.out.println("2. VIP");
+            System.out.println("3. VVIP");
+            System.out.print("Pilih jenis tiket (1/2/3): ");
+            int jenisTiket = scanner.nextInt();
+            scanner.nextLine();
+
+            if(jenisTiket != 1 && jenisTiket != 2 && jenisTiket != 3){
+                System.out.println("\nInput tidak valid!");
+            } else{
+                System.out.print("\nMasukan jumlah jenis tiket yang ingin dibeli: ");
+                int jumlahTiket = scanner.nextInt();
+
+                Tiket tiket;
+                if (jenisTiket == 1) {
+                    tiket = new Festival();
+                    Pemesanan pemesanan = new Pemesanan(nama, "Reguler", tiket, jumlahTiket);
+                    pemesanan.cetakNota();
+                } else if (jenisTiket == 2) {
+                    tiket = new VIP();
+                    Pemesanan pemesanan = new Pemesanan(nama, "Reguler", tiket, jumlahTiket);
+                    pemesanan.cetakNota();
+                } else if (jenisTiket == 3) {
+                    tiket = new VVIP();
+                    Pemesanan pemesanan = new Pemesanan(nama, "Reguler", tiket, jumlahTiket);
+                    pemesanan.cetakNota();
+                } 
+            }
+              
+
+        } else {
+            System.out.println("\nInput tidak valid!");
+        }  
+
+
     }
 }
